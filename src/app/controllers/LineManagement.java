c73df044c49e518c5fb659ed5d5f677f23738a50
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2020-05-10 17:03:40 +0200

    Asi double kurva

--- src/app/controllers/LineManagement.java
+++ src/app/controllers/LineManagement.java
@@ -1,14 +1,27 @@
 package app.controllers;
 
 import app.models.maps.Line;
+import app.models.maps.Street;
+import javafx.application.Platform;
 import javafx.scene.control.ListView;
 
 public class LineManagement {
     public ListView newRouteListView;
     public ListView selectStreetListView;
+    public ListView currentRouteListView;
     private Line line;
 
-    public void startUp(Line line){
-        this.line=line;
+    public void startUp(Line line) {
+        this.line = line;
+        refreshGui();
+    }
+
+    private void refreshGui() {
+        Platform.runLater(() -> {
+            currentRouteListView.getItems().clear();
+            for (Street street : line.getStreets()) {
+                currentRouteListView.getItems().add(street.getId());
+            }
+        });
     }
 }
